351d6ef4599f1dfe82b02ba2d8a7e11a843ce850
Vitor Dalvi <vitodalvi@gmail.com>
2025-08-22 17:23:49 -0300

    006 done.

---
+++ 006/Program.cs
@@ -0,0 +1,39 @@
+﻿// Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        bool status = true;
+        double PI = Math.PI;
+
+        while (status)
+        {
+            Console.Write("Digite o raio do círculo: ");
+
+            if (!double.TryParse(Console.ReadLine(), out double radius))
+            {
+                Console.WriteLine("Valor inválido. Tente novamente!");
+            }
+
+            double area = PI * Math.Pow(radius, 2);
+
+            Console.WriteLine("O valor da área é: " + area.ToString("0.00"));
+
+            Console.Write("Você deseja fazer mais conversões? (S/N): ");
+            string op = Console.ReadLine();
+
+            switch (op)
+            {
+                case "s":
+                    status = true;
+                    break;
+
+                case "n":
+                    Console.WriteLine("\nPrograma finalizado.");
+                    status = false;
+                    break;
+            }
+        }
+    }
+}
