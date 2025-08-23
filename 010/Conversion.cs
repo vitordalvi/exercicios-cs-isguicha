869ffbd0ed564639d27ac9b768df6010f95db44f
Vitor Dalvi <vitodalvi@gmail.com>
2025-08-23 09:24:40 -0300

    added 010.

---
+++ 010/Conversion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _010
+{
+    public class Conversion
+    {
+        public double CelciusToFahrenheit(double celcius)
+        {
+            return (celcius * (9 / 5)) + 32;
+        }
+    }
+}
---
+++ 010/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+Faça um Programa que peça a temperatura em graus Celsius,
+transforme e mostre em graus Fahrenheit.
+*/
+
+namespace _010
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Conversion convert = new Conversion();
+            bool status = true;
+
+            while (status)
+            {
+                Console.Write("Digite a quantidade em celcius: ");
+
+                if (!double.TryParse(Console.ReadLine(), out double celcius))
+                {
+                    Console.WriteLine("O valor é inválido. Tente novamente!");
+                }
+
+                double res = convert.CelciusToFahrenheit(celcius);
+
+                Console.WriteLine("O valor de " + celcius + "C° é: " + res + "F°.");
+
+                Console.Write("Você deseja fazer mais conversões? (S/N): ");
+                string op = Console.ReadLine();
+
+                switch (op)
+                {
+                    case "s":
+                        status = true;
+                        break;
+
+                    case "n":
+                        Console.WriteLine("\nPrograma finalizado!");
+                        status = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        break;
+                }
+            }
+        }
+    }
+}
