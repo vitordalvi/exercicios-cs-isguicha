0608f4b7fe825f1354070f8e7b19fde14f2c16f9
Vitor Dalvi <vitodalvi@gmail.com>
2025-08-25 13:51:56 -0300

    012 done.

---
+++ 012/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _012
+{
+    public class Calculator
+    {
+        public double CalcWeight()
+        {
+            if (!double.TryParse(Console.ReadLine(), out double weight))
+            {
+                throw new FormatException("O valor inserido é inválido. Tente novamente!");
+            }
+
+            return (72.7 * weight) - 58;
+
+            } 
+    }
+}
---
+++ 012/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Tendo como dados de entrada a altura de uma pessoa,
+construa um algoritmo que calcule seu peso ideal,
+usando a seguinte fórmula: (72.7*altura) - 58
+*/
+
+using _012;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        bool status = true;
+        Calculator calc = new Calculator();
+
+        while (status)
+        {
+            Console.Write("Digite o valor da altura em centímetros: ");
+            double weight = calc.CalcWeight();
+
+            Console.WriteLine($"O peso ideal para sua altura é: {weight}Kg.");
+
+            Console.Write("Você deseja fazer mais cálculos? (S/N): ");
+            string op = Console.ReadLine().ToLowerInvariant();
+
+            switch (op)
+            {
+                case "s":
+                    status = true;
+                    break;
+
+                case "n":
+                    Console.WriteLine("\nPrograma finalizado!");
+                    status = false;
+                    break;
+
+                default:
+                    Console.WriteLine("Valor inválido.");
+                    break;
+            }
+        }
+    }
+}
